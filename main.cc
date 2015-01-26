072919f18da87a9dbbf49929870ab4892de404df
Kévin Sztern <sztern.kevin@gmail.com>
2015-01-26 03:42:01 +0100

    first 3D implementation, horrible CPU logic handling

--- main.cc
+++ main.cc
@@ -1,24 +1,30 @@
+#define GLM_FORCE_RADIANS
 #include <vector>
 #include <iostream>
 #include <GL/glew.h>
 #include <SFML/Window.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/random.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-const size_t num_vertices = 10000;
+const size_t num_vertices = 100000;
 
 const GLchar* vertex_shader = R"SHADER(
 #version 150 core
 
-in vec2 position;
+in vec3 position;
 in vec3 color;
+
+uniform mat4 view;
+uniform mat4 projection;
 
 out vec3 Color;
 
 void main()
 {
     Color = color;
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = projection * view * vec4(position, 1.0);
 }
 )SHADER";
 
@@ -36,7 +42,7 @@ void main()
 
 struct vertex
 {
-    glm::vec2 pos;
+    glm::vec3 pos;
     glm::vec3 color;
 };
 
@@ -58,9 +64,13 @@ int main()
 
     for (auto& vertex : vertices)
     {
-	vertex.pos = glm::gaussRand(glm::vec2(0), glm::vec2(0.7));
+	vertex.pos = glm::gaussRand(glm::vec3(0), glm::vec3(0.7));
 	vertex.color = glm::linearRand(glm::vec3(0), glm::vec3(1));
     }
+
+    std::vector<glm::vec3> velocities(num_vertices);
+    for (auto& v : velocities)
+	v = glm::linearRand(glm::vec3(-1), glm::vec3(1));
 
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -69,7 +79,7 @@ int main()
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof (vertex), vertices.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof (vertex), vertices.data(), GL_DYNAMIC_DRAW);
 
     GLuint v_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(v_shader, 1, &vertex_shader, NULL);
@@ -101,14 +111,30 @@ int main()
 
     GLint attrib_pos = glGetAttribLocation(program, "position");
     glEnableVertexAttribArray(attrib_pos);
-    glVertexAttribPointer(attrib_pos, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0);
+    glVertexAttribPointer(attrib_pos, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), 0);
     
     GLint attrib_color = glGetAttribLocation(program, "color");
     glEnableVertexAttribArray(attrib_color);
-    glVertexAttribPointer(attrib_color, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(2*sizeof(float)));
+    glVertexAttribPointer(attrib_color, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
 
+    glm::mat4 projection = glm::perspective(45.f, 800.f / 600.f, 1.f, 10.f);
+    GLint uni_proj = glGetUniformLocation(program, "projection");
+    glUniformMatrix4fv(uni_proj, 1, GL_FALSE, glm::value_ptr(projection));
+
+    glm::mat4 view = glm::lookAt(
+	    glm::vec3(5.f, 0.f, 0.f),
+	    glm::vec3(0.f, 0.f, 0.f),
+	    glm::vec3(0.f, 0.f, 1.f));
+
+    sf::Clock clock;
     while (window.isOpen())
     {
+	sf::Time elapsed = clock.restart();
+	view = glm::rotate(view, elapsed.asSeconds(), glm::vec3(0.f, 0.f, 1.f));
+	GLint uni_view = glGetUniformLocation(program, "view");
+	glUniformMatrix4fv(uni_view, 1, GL_FALSE, glm::value_ptr(view));
+
+	std::cout << 1.f / elapsed.asSeconds() << std::endl;
 
 	glClearColor(0.f, 0.f, 0.f, 0.f);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -116,6 +142,42 @@ int main()
 
 	window.display();
 
+	for (size_t i = 0; i < vertices.size(); ++i)
+	{
+	    vertices[i].pos = vertices[i].pos + velocities[i] * elapsed.asSeconds();
+	    if (vertices[i].pos.x > 1.3f)
+	    {
+		vertices[i].pos.x = 1.3f;
+		velocities[i].x *= -1;
+	    }
+	    if (vertices[i].pos.x < -1.3f)
+	    {
+		vertices[i].pos.x = 1.3f;
+		velocities[i].x *= -1;
+	    }
+	    if (vertices[i].pos.y > 1.3f)
+	    {
+		vertices[i].pos.y = 1.3f;
+		velocities[i].y *= -1;
+	    }
+	    if (vertices[i].pos.y < -1.3f)
+	    {
+		vertices[i].pos.y = 1.3f;
+		velocities[i].y *= -1;
+	    }
+	    if (vertices[i].pos.z > 1.3f)
+	    {
+		vertices[i].pos.z = 1.3f;
+		velocities[i].z *= -1;
+	    }
+	    if (vertices[i].pos.z < -1.3f)
+	    {
+		vertices[i].pos.z = 1.3f;
+		velocities[i].z *= -1;
+	    }
+	}
+
+	glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof (vertex), vertices.data());
 	sf::Event event;
 	while (window.pollEvent(event))
 	{
