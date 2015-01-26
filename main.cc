7d8a1c2aaeb5698c45104102250e39da107d7e24
Kévin Sztern <sztern.kevin@gmail.com>
2015-01-26 02:41:57 +0100

    Generating random distribution of points, still 2D

--- main.cc
+++ main.cc
@@ -1,6 +1,11 @@
+#include <vector>
 #include <iostream>
 #include <GL/glew.h>
 #include <SFML/Window.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/random.hpp>
+
+const size_t num_vertices = 10000;
 
 const GLchar* vertex_shader = R"SHADER(
 #version 150 core
@@ -29,6 +34,12 @@ void main()
 }
 )SHADER";
 
+struct vertex
+{
+    glm::vec2 pos;
+    glm::vec3 color;
+};
+
 int main()
 {
     sf::ContextSettings settings;
@@ -38,15 +49,18 @@ int main()
 
     sf::Window window{sf::VideoMode{800, 600}, "OpenGL", sf::Style::Close, settings};
 
+
     glewExperimental = GL_TRUE;
     glewInit();
 
 
-    float vertices[] = {
-	0.f, 0.5f, 1.f, 0.f, 0.f,
-	0.5f, -0.5f, 0.f, 1.f, 0.f,
-	-0.5f, -0.f, 0.f, 0.f, 1.f
-    };
+    std::vector<vertex> vertices(num_vertices);
+
+    for (auto& vertex : vertices)
+    {
+	vertex.pos = glm::gaussRand(glm::vec2(0), glm::vec2(0.7));
+	vertex.color = glm::linearRand(glm::vec3(0), glm::vec3(1));
+    }
 
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -55,7 +69,7 @@ int main()
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof (vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof (vertex), vertices.data(), GL_STATIC_DRAW);
 
     GLuint v_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(v_shader, 1, &vertex_shader, NULL);
@@ -98,7 +112,7 @@ int main()
 
 	glClearColor(0.f, 0.f, 0.f, 0.f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_POINTS, 0, num_vertices);
 
 	window.display();
 
