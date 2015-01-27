d7be845d09e310a7e821893987b12e4d02c0e372
Kévin Sztern <sztern.kevin@gmail.com>
2015-01-27 22:13:57 +0100

    compute shader in place

--- main.cc
+++ main.cc
@@ -13,37 +13,70 @@ const size_t num_particles = 100000;
 const GLchar* vertex_shader = R"SHADER(
 #version 430
 
-in vec3 position;
-in vec3 color;
+in vec4 position;
+in vec4 color;
 
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 Color;
+out vec4 Color;
 
 void main()
 {
     Color = color;
-    gl_Position = projection * view * vec4(position, 1.0);
+    gl_Position = projection * view * vec4(position.xyz, 1.0);
 }
 )SHADER";
 
 const GLchar* fragment_shader = R"SHADER(
 #version 430
 
-in vec3 Color;
+in vec4 Color;
 out vec4 outColor;
 
 void main()
 {
-    outColor = vec4(Color, 1.0);
+    outColor = vec4(Color.xyz, 1.0);
+}
+)SHADER";
+
+
+
+const GLchar* compute_shader = R"SHADER(
+#version 430
+#extension GL_ARB_compute_shader : enable
+#extension GL_ARB_shader_storage_buffer_object : enable
+
+struct particle
+{
+    vec4 pos;
+    vec4 color;
+};
+
+layout(std430, binding=0) buffer particles
+{
+    particle p[];
+};
+
+layout(std430, binding=1) buffer velocities
+{
+    vec4 v[];
+};
+
+uniform float elapsed_time;
+
+layout (local_size_x = 256, local_size_y = 1, local_size_z = 1) in;
+void main()
+{
+    uint gid = gl_GlobalInvocationID.x;
+    p[gid].pos.xyz += v[gid].xyz * elapsed_time;
 }
 )SHADER";
 
 struct vertex
 {
-    glm::vec3 pos;
-    glm::vec3 color;
+    glm::vec4 pos;
+    glm::vec4 color;
 };
 
 int main()
@@ -63,43 +96,37 @@ int main()
     // 0: vertex position and color
     // 1: vertex velocity
     // Velocity is in a different SSBO, because the rendering program doesn't need it
-    GLuint ssbo;
-    glGenBuffers(1, &ssbo);
+    GLuint ssbos[2];
+    glGenBuffers(1, ssbos);
 
     // Initialize positions and colors randomly
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbos[0]);
     glBufferData(GL_SHADER_STORAGE_BUFFER, num_particles * sizeof (vertex),
                  NULL, GL_STATIC_DRAW);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbos[0]);
     vertex* positions = (vertex*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER,
                                                         0, num_particles * sizeof (vertex),
                                                         GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
 
     for (size_t i = 0; i < num_particles; ++i)
     {
-        positions[i].pos = glm::gaussRand(glm::vec3(0), glm::vec3(0.7));
-        positions[i].color = glm::linearRand(glm::vec3(0), glm::vec3(1));
+        positions[i].pos = glm::gaussRand(glm::vec4(0), glm::vec4(0.7));
+        positions[i].color = glm::linearRand(glm::vec4(0), glm::vec4(1));
     }
     glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 
 
-#if 0
-    // Initialize velocity 
+    // Initialize velocity
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbos[1]);
-    glBufferData(GL_SHADER_STORAGE_BUFFER, num_particles * sizeof (glm::vec3),
+    glBufferData(GL_SHADER_STORAGE_BUFFER, num_particles * sizeof (glm::vec4),
                  NULL, GL_STATIC_DRAW);
-    glm::vec3* velocities = (glm::vec3*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER,
-                                                        0, num_particles * sizeof (glm::vec3),
+    glm::vec4* velocities = (glm::vec4*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER,
+                                                        0, num_particles * sizeof (glm::vec4),
                                                         GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
     for (size_t i = 0; i < num_particles; ++i)
-        velocities[i] = glm::linearRand(glm::vec3(0), glm::vec3(1));
+        velocities[i] = glm::linearRand(glm::vec4(0), glm::vec4(1));
     glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-#endif
 
-
-    /****************************************************/
-    // RENDER PROGRAM
-    /****************************************************/
 
     // Compiling the vertex shader
     GLuint v_shader = glCreateShader(GL_VERTEX_SHADER);
@@ -135,6 +162,28 @@ int main()
     // Everything Specified, time to link!
     glLinkProgram(render_program);
 
+    /****************************************************/
+    // COMPUTE PROGRAM
+    /****************************************************/
+
+    GLuint c_shader = glCreateShader(GL_COMPUTE_SHADER);
+    glShaderSource(c_shader, 1, &compute_shader, NULL);
+    glCompileShader(c_shader);
+
+    glGetShaderiv(f_shader, GL_COMPILE_STATUS, &status);
+    std::cout << status << std::endl;
+
+    // If error occured, print the compiling log
+    glGetShaderInfoLog(v_shader, 512, NULL, buffer);
+    std::cout << buffer << std::endl;
+
+    GLuint compute_program = glCreateProgram();
+    glAttachShader(compute_program, c_shader);
+    glLinkProgram(compute_program);
+    glGetProgramInfoLog(compute_program, 512, NULL, buffer);
+    std::cout << buffer << std::endl;
+
+
 
     // Initialize VAO
     GLuint vao;
@@ -143,7 +192,7 @@ int main()
 
     glUseProgram(render_program);
     // Specifying how to access attributes from buffer object
-    glBindBuffer(GL_ARRAY_BUFFER, ssbo);
+    glBindBuffer(GL_ARRAY_BUFFER, ssbos[0]);
 
     GLint attrib_pos = glGetAttribLocation(render_program, "position");
     GLint attrib_color = glGetAttribLocation(render_program, "color");
@@ -151,16 +200,13 @@ int main()
     glEnableVertexAttribArray(attrib_color);
     glEnableVertexAttribArray(attrib_pos);
 
-    glVertexAttribPointer(attrib_pos, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), 0);
-    glVertexAttribPointer(attrib_color, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glVertexAttribPointer(attrib_pos, 4, GL_FLOAT, GL_FALSE, 8*sizeof(float), 0);
+    glVertexAttribPointer(attrib_color, 4, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(4*sizeof(float)));
 
     // Initialize render program uniforms
     glm::mat4 projection = glm::perspective(45.f, 800.f / 600.f, 1.f, 10.f);
     GLint uni_proj = glGetUniformLocation(render_program, "projection");
     glUniformMatrix4fv(uni_proj, 1, GL_FALSE, glm::value_ptr(projection));
-
-        if (glGetError())
-            std::cout << "BITE" << std::endl;
 
 
     glm::mat4 view = glm::lookAt(
@@ -168,19 +214,36 @@ int main()
             glm::vec3(0.f, 0.f, 0.f),
             glm::vec3(0.f, 0.f, 1.f));
 
+    GLint uni_view = glGetUniformLocation(render_program, "view");
+    glUniformMatrix4fv(uni_view, 1, GL_FALSE, glm::value_ptr(view));
     sf::Clock clock;
+    int i = 0;
     while (window.isOpen())
     {
+        std::cout << i++ << std::endl;
+
         sf::Time elapsed = clock.restart();
-        view = glm::rotate(view, elapsed.asSeconds(), glm::vec3(0.f, 0.f, 1.f));
-        GLint uni_view = glGetUniformLocation(render_program, "view");
-        glUniformMatrix4fv(uni_view, 1, GL_FALSE, glm::value_ptr(view));
 
         //std::cout << 1.f / elapsed.asSeconds() << std::endl;
 
-        glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
+        if (auto bite = glGetError())
+        {
+            std::cout << bite << std::endl;
+            break;
+        }
+        glUseProgram(compute_program);
+        GLint uni_time = glGetUniformLocation(compute_program, "elapsed_time");
+        glUniform1f(uni_time, elapsed.asSeconds());
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbos[0]);
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssbos[1]);
+        glDispatchCompute((num_particles / 256) + 1, 1, 1);
+        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT | GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT);
 
         // RENDER TIME
+        glUseProgram(render_program);
+        view = glm::rotate(view, elapsed.asSeconds(), glm::vec3(0.f, 0.f, 1.f));
+        GLint uni_view = glGetUniformLocation(render_program, "view");
+        glUniformMatrix4fv(uni_view, 1, GL_FALSE, glm::value_ptr(view));
         glClearColor(0.f, 0.f, 0.f, 0.f);
         glClear(GL_COLOR_BUFFER_BIT);
         glEnableClientState(GL_VERTEX_ARRAY);
